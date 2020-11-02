b75db70d3aca390eb6090102b18aa8c4d2d33944
Aaron Elam <speztra97@gmail.com>
2020-11-02 13:14:56 -0600

    fcfs refactored

--- src/cpuscheduler/FCFS.java
+++ src/cpuscheduler/FCFS.java
@@ -39,82 +39,83 @@ public class FCFS extends Scheduler {
             LinkedList<Process> rdQueue = new LinkedList<>();
             clock = event.getTime();
 
-            /*
-             *
-             * Arrival event
-             *
-             */
-            if (event.getType() == ARRIVAL) {
-                // nothing on cpu
-                if (cpuIdle) {
-                    cpuIdle = false;
-                    cpuIdleTime += clock - lastTimeCpuBusy;
-                    event.getProcess().setLastTimeOnCpu(clock);
-                    // assign to cpu
-                    onCpu = event.getProcess();
-
-                    // schedule this process for departure
-                    eventScheduler.scheduleEvent((clock + event.getProcess().getRemainingBurst()), event.getProcess(),
-                            DEPARTURE);
-                }
-                // cpu not idle
-                if(!cpuIdle){
-                    //TODO: sort from lowest service time to highest. Low gets priority
-                    rdQueue.add(event.getProcess());
-                    totalReadyQueueProcesses++;
-                    rdQueue.sort(event.getProcess());
-                }
-                Process nextProcess = new Process(event.getProcess().getId() + 1, clock + genexp(arrivalRate),
-                        genexp(1 / serviceTime));
-                processes.add(nextProcess);
-
-                // send this process off to arrive
-                eventScheduler.scheduleEvent(nextProcess.getArrivalTime(), nextProcess, ARRIVAL);
-
-                /*
-                 *
-                 * Departure event
-                 *
-                 */
-            } if (event.getType() == DEPARTURE) {
-                // stat updates
-                event.getProcess().setCompletionTime(clock);
-                event.getProcess().setRemainingBurst(0);
-                processesSimulated++;
-
-                // place a waiting process in the ready queue then assign to cpu
-                if (!rdQueue.isEmpty()) {
-                    Process processInQueue = rdQueue.peekFirst();
-                    onCpu = processInQueue;
-
-                    // update times
-                    onCpu.setLastTimeOnCpu(clock);
-                    onCpu.setWaitTime(clock - processInQueue.getArrivalTime());
-
-                    // schedule this process for departure
-                    eventScheduler.scheduleEvent(clock + processInQueue.getBurst(), processInQueue, DEPARTURE);
-
-                }
-                // ready queue is empty
-                else {
-                    lastTimeCpuBusy = clock;
-                    cpuIdle = true;
-                    onCpu = null;
-                }
-            }
-
-            /*
-             * Query event update rq stats
-             */
-            else if (event.getType() == QUERY) {
-                totalReadyQueueProcesses += rdQueue.size();
-                eventScheduler.scheduleEvent(clock + queryInterval, null, QUERY);
+            switch(event.getType()) {
+                case ARRIVAL -> arrival(eventScheduler, event, rdQueue, clock);
+                case DEPARTURE -> departure(eventScheduler, event, rdQueue, clock);
+                case QUERY -> query(eventScheduler, event, rdQueue, clock);
             }
         }
+        return fcfsStats();
+    }
 
-        /*
-         * Turnaround time stats
-         */
+    /*
+     Methods section
+     */
+    public void arrival(EventScheduler eventScheduler, Event event, LinkedList<Process> rdQueue, float clock) {
+        // nothing on cpu
+        if (cpuIdle) {
+            cpuIdle = false;
+            cpuIdleTime += clock - lastTimeCpuBusy;
+            event.getProcess().setLastTimeOnCpu(clock);
+            // assign to cpu
+            onCpu = event.getProcess();
+
+            // schedule this process for departure
+            eventScheduler.scheduleEvent((clock + event.getProcess().getRemainingBurst()), event.getProcess(),
+                    DEPARTURE);
+        }
+        // cpu not idle
+        if(!cpuIdle){
+            //TODO: sort from lowest service time to highest. Low gets priority
+            rdQueue.add(event.getProcess());
+            totalReadyQueueProcesses++;
+            rdQueue.sort(event.getProcess());
+        }
+        Process nextProcess = new Process(event.getProcess().getId() + 1, clock + genexp(arrivalRate),
+                genexp(1 / serviceTime));
+        processes.add(nextProcess);
+
+        // send this process off to arrive
+        eventScheduler.scheduleEvent(nextProcess.getArrivalTime(), nextProcess, ARRIVAL);
+    }
+
+    public void departure(EventScheduler eventScheduler, Event event, LinkedList<Process> rdQueue, float clock) {
+        // stat updates
+        event.getProcess().setCompletionTime(clock);
+        event.getProcess().setRemainingBurst(0);
+        processesSimulated++;
+
+        // place a waiting process in the ready queue then assign to cpu
+        if (!rdQueue.isEmpty()) {
+            Process processInQueue = rdQueue.peekFirst();
+            onCpu = processInQueue;
+
+            // update times
+            onCpu.setLastTimeOnCpu(clock);
+            onCpu.setWaitTime(clock - processInQueue.getArrivalTime());
+
+            // schedule this process for departure
+            eventScheduler.scheduleEvent(clock + processInQueue.getBurst(), processInQueue, DEPARTURE);
+
+        }
+        // ready queue is empty
+        else {
+            lastTimeCpuBusy = clock;
+            cpuIdle = true;
+            onCpu = null;
+        }
+    }
+
+    public void query(EventScheduler eventScheduler, Event event, LinkedList<Process> rdQueue, float clock) {
+        totalReadyQueueProcesses += rdQueue.size();
+        eventScheduler.scheduleEvent(clock + queryInterval, null, QUERY);
+    }
+
+
+    /*
+     * Turnaround time stats
+     */
+    public Stats fcfsStats() {
         float totalTurnaroundTime = 0;
         for (Process process : processes) {
             if (process.getCompletionTime() != -1) {
