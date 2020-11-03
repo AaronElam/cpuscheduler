532dc8cb578643de918e566c6f0610afcbc46647
Aaron Elam <speztra97@gmail.com>
2020-11-02 19:11:25 -0600

    sh

--- sim.sh
+++ sim.sh
@@ -2,6 +2,6 @@
 
 for((i = 10; i < 31; i++));
 do
-  ./Main 1 $i 0.04 0.01
-  cp sim.data /data/1-$i-004.data
+  java ./src/cpuscheduler/Main 1 $i 0.04 0.01
+  cp ./Data/sim.data ./Data/1-$i-004.data
 done
